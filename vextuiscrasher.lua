b17f1d2432dd819fa0401d7636b8f9a905cfd72d
banov <100329389+bannywanny@users.noreply.github.com>
2024-11-19 04:21:02 -0600

    Update vextuiscrasher.lua

--- vextuiscrasher.lua
+++ vextuiscrasher.lua
@@ -1,6 +1,6 @@
 local args = {
     [1] = false,
-    [2] = game:GetService("Players").LocalPlayer:WaitForChild("Linked Sword")
+    [2] = game:GetService('Players').LocalPlayer.Character.Humanoid:FindFirstChildOfClass('Tool')
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("HotbarRemotes"):WaitForChild("ToolEvent"):FireServer(unpack(args))
