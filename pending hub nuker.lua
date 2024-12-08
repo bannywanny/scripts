b79a489d6d00f615cc677a2d766f66bda461157c
banov <100329389+bannywanny@users.noreply.github.com>
2024-12-08 02:59:29 -0600

    Update pending hub nuker.lua

--- pending hub nuker.lua
+++ pending hub nuker.lua
@@ -2,16 +2,31 @@
 nukes pending hubs of games with infinite rejoins lol 
 set in autoexec and use multi-instance for stronger nuking
 ratelimits their webhook after a while meaning noone can join the event W
+press q to stop rejoining
 --]]
 
 
---[[local Players = game:GetService("Players")
+local Players = game:GetService("Players")
 local TeleportService = game:GetService("TeleportService")
+local UserInputService = game:GetService("UserInputService")
 local PlaceId = game.PlaceId
 local JobId = game.JobId
 local player = Players.LocalPlayer
+local rejoining = true
+
+local function oninput(input, gameProcessed)
+    if input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode.Q then
+        rejoining = false
+    end
+end
+
+UserInputService.InputBegan:Connect(oninput)
 
 while true do
+    if not rejoining then
+        break
+    end
+    
     local g = Players:GetChildren()
     if #g >= 1 then
         player:Kick("v64")
@@ -22,4 +37,4 @@ while true do
     end
     
     wait(0.5)
-end--]]
+end
