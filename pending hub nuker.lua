be4128d7ae44bdacf5f416df76f684a3bf47c593
banov <100329389+bannywanny@users.noreply.github.com>
2024-12-08 03:17:18 -0600

    Update pending hub nuker.lua

--- pending hub nuker.lua
+++ pending hub nuker.lua
@@ -1,3 +1,9 @@
+--[[
+nukes pending hubs to ratelimit and ruin events
+put in autoexec
+if you're on codex i wouldnt use this if i were u cuz youll get in a infinite rejoin loop if u put this in autoexec
+--]]
+
 local Players = game:GetService("Players")
 local TeleportService = game:GetService("TeleportService")
 local PlaceId = game.PlaceId
