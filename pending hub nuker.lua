518bbb6ffde42259e29f028ca873195205dce75d
banov <100329389+bannywanny@users.noreply.github.com>
2024-12-08 03:14:44 -0600

    Update pending hub nuker.lua

--- pending hub nuker.lua
+++ pending hub nuker.lua
@@ -1,1 +1,18 @@
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+local PlaceId = game.PlaceId
+local JobId = game.JobId
+local player = Players.LocalPlayer
 
+while true do
+    local g = Players:GetChildren()
+    if #g >= 1 then
+        player:Kick("v64")
+        wait()
+        TeleportService:Teleport(PlaceId, player)
+    else
+        TeleportService:TeleportToPlaceInstance(PlaceId, JobId, player)
+    end
+    
+    wait(0.5)
+end
